e9bccba5371325abeb369cdc7097df2a0581bea0
queolvi <korpachyovdmitriy@gmail.com>
2025-01-21 22:16:39 +0300

    frst and scnd exrss done in HW-15

--- HWORKS/HW-15/2/main.cpp
+++ HWORKS/HW-15/2/main.cpp
@@ -9,14 +9,9 @@ int Age_;
 std::cout << "Enter a: students surname, name, patronymic, his group and age: \n";
 std::cin >> Surname_ >> Name_ >> Patronymic_ >> Group_ >> Age_;
 
-Student student = Student::CreateSudent_(Surname_, Name_, Patronymic_, Group_, Age_); 
+Student student = Student::CreateStudent_(Surname_, Name_, Patronymic_, Group_, Age_);
 
-std::cout << "\n\nInformation about studemt: \n" <<
-"Surname - " << student.Surname_<< "\n" <<
-"Name - " << student.Name_ << "\n" <<
-"Patronymic - " << student.Patronymic_ << "\n" <<
-"Group - " << student.Group_ << "\n" <<
-"Age - " << student.Age_ << "\n";  
+std::cout << student;
 
     return 0;
 }
--- HWORKS/HW-15/2/student.hpp
+++ HWORKS/HW-15/2/student.hpp
@@ -4,14 +4,11 @@ class Student {
     std::string Surname_, Name_, Patronymic_, Group_;
     int Age_;
 
-    static Student CreateSudent_(const std::string& Surname_,
-    const std::string& Name_,
-    const std::string& Patronymic_,
-    const std::string& Group_,
-    const int& Age_) 
-    { return Student(Surname_, Name_, Patronymic_, Group_, Age_); }
 
-    private:
+    //конструктор по умолчанию
+    Student() : Age_(0) {}   
+
+    //конструктор с параметрами
     Student(const std::string& Surname_,
     const std::string& Name_,
     const std::string& Patronymic_,
@@ -23,4 +20,30 @@ class Student {
     Group_(Group_), 
     Age_(Age_) 
     {}
+
+    //конструктор копирования - разрешение дано(дефолтом)
+    Student(const Student& other) = default;
+
+    //оператор присваивания копированием - разрешение дано(дефолтом)
+    Student& operator = (const Student& other) = default; 
+
+    //статический метод для создания объекта
+    static Student CreateStudent_(const std::string& Surname_,
+    const std::string& Name_,
+    const std::string& Patronymic_,
+    const std::string& Group_,
+    int Age_) 
+    { return Student(Surname_, Name_, Patronymic_, Group_, Age_); }
+
+    //получаение доступа оператору (operator <<) к классу Student, который не является членом класса Student
+    friend std::ostream& operator <<(std::ostream & os, const Student& s) {
+    os << "Surname - " << s.Surname_<< "\n";
+    os << "Name - " << s.Name_ << "\n";
+    os << "Patronymic - " << s.Patronymic_ << "\n";
+    os << "Group - " << s.Group_ << "\n";
+    os << "Age - " << s.Age_ << "\n";
+    return os;
+    }
+
+
 };
