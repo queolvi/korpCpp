5f9f5959dfc990979048cc184b9d09d1d0df4719
queolvi <korpachyovdmitriy@gmail.com>
2025-02-11 15:56:28 +0300

    HW 16.1, recieved the update

--- HWORKS/HW-16/acid_solution.hpp
+++ HWORKS/HW-16/acid_solution.hpp
@@ -1,5 +1,14 @@
-class acid_solution {
+#pragma once
+#include <utility>
+namespace HW{
+class Acid_solution {
     private:
+    int value;
+    double material;
 
     public:
+    Acid_solution(int volume = 0, int percent = 100);
+    std::pair<int, double> getter () const;
+    double calculate_concentration(const Acid_solution& first_sol, const Acid_solution& second_sol);
 };
+}
