47d131717d109ba84f266176d6f0ec3b90bab49d
queolvi <korpachyovdmitriy@gmail.com>
2025-02-28 16:53:02 +0300

    comm

--- training_cpp/real_numbers/27/27.cpp
+++ training_cpp/real_numbers/27/27.cpp
@@ -2,6 +2,7 @@
 #include<cmath>
 int main() {
 
+	#ifdef AAA
 	double a = 0, a_pow = 0, a_for_null = 0;
 	std::cout << "Enter num: "; std::cin >> a;
 
@@ -16,7 +17,17 @@ int main() {
 	std::cout << "Your num^1 * 0 : " << a << "\n";
 	a_for_null = std::pow(a_for_null, 1) * 5;
 	std::cout << "Your num^1 * 5" << a_for_null;
-	//3
-
+	#endif
+	
+	//#ifdef BBB
+	std::cout << "Enter num: ";
+	long long a = 0; std::cin >> a;
+	std::cout << "\n\n";
+	long long pow_2 = a * a;	std::cout << pow_2 << "\n";
+	long long pow_3 = pow_2 * a;	std::cout << pow_3 << "\n";
+	long long pow_5 = pow_2 * pow_3;	std::cout << pow_5 << "\n";
+	long long pow_1_mult_5 = a * 1 * 5;	std::cout << pow_1_mult_5 << "\n";
+	//#endif
+	
 	return 0;
 }
