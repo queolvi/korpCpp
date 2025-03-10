8123bcdfd208827b9fecefe60e96b70e474aadf4
queolvi <korpachyovdmitriy@gmail.com>
2025-03-10 11:16:13 +0300

    comm

---
+++ training_cpp/real_numbers/34/34.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+
+    double V_calm = 0;
+    double U_river = 0;
+    
+    std::cout << "Enter speed of a boat in still water and river flow speed: \n";
+    std::cin >> V_calm >> U_river;
+
+    if(V < U) {
+    std::cout << "Invalid values!! \n";
+    return 1;
+    }
+
+    double boat_travel_time_on_the_lake = 0;
+    double time_against_the_current = 0;
+
+    std::cout << "Enter boat travel time on the lake and time against the current: \n";
+    std::cin >> boat_travel_rime_on_the_lake >> time_against_the_current;
+
+    
+
+    return 0;
+}
