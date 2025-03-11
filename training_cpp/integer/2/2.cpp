190fde7a73534774a654167f4e6bdcdb1d6a0d3d
queolvi <korpachyovdmitriy@gmail.com>
2025-03-12 01:01:23 +0300

    comm

--- training_cpp/integer/2/2.cpp
+++ training_cpp/integer/2/2.cpp
@@ -12,6 +12,24 @@ int main () {
         return 1;
     }
     int Ton = Mass / 1000;
+
+    std::cout << Mass << " kg to Ton == " << Ton << "\n";
+
+
+    if(Mass >= 0) {
+        std::cout << "do you wanna check, if result == 0, maybe he nust be 0.1-0.9 ?\n";
+        std::cout << "if you agree, enter 1, or 0 if not: \n";
+        bool choice;
+        std::cin >> choice;
+        if(choice == true) {
+            Mass = static_cast<double>(Mass);
+            Ton = static_cast<double>(Ton);
+            
+            std::cout << "Result: " << Ton << "\n";
+        } else if(choice == false) {
+            std::cout << "You reject changes. \n";
+        }
+    }
     
-    return 1;
+    return 0;
 }
