8f459b855c41bf93971ee21105a7d69b2814c4b5
thekorp <146642604+queolvi@users.noreply.github.com>
2025-04-02 10:54:06 +0300

    0/5

--- second.sh
+++ second.sh
@@ -1,3 +1,4 @@
 find . -type f -printf "%s %p\n" | sort -n | head -n 1 # Самый маленький файл (размер, путь)
 find . -type f -printf "%s %p\n" | sort -nr | head -n 1 # Самый большой файл (размер, путь)
 
+https://www.avito.ru/balashiha/noutbuki/thinkpad_v_zaryazhennoy_versiey_64gb_ozu_i_ssd_m2_3950630144?slocation=662810&context=H4sIAAAAAAAA_wEmANn_YToxOntzOjE6IngiO3M6MTY6Ijl6RjFNSHZ5TGQ4UWFFd1giO300f6ImJgAAAA
